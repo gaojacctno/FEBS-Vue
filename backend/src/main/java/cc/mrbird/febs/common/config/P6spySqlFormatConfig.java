762921da7974883350661ba004f523d48848710e
jamie <214142603@qq.com>
2019-09-07 22:03:12 +0800

    format fix

--- backend/src/main/java/cc/mrbird/febs/common/config/P6spySqlFormatConfig.java
+++ backend/src/main/java/cc/mrbird/febs/common/config/P6spySqlFormatConfig.java
@@ -7,14 +7,14 @@ import org.apache.commons.lang3.StringUtils;
 import java.time.LocalDateTime;
 
 /**
- * 自定义 p6spy sql输出格式
+  * 自定义 p6spy sql输出格式
  *
  * @author MrBird
  */
 public class P6spySqlFormatConfig implements MessageFormattingStrategy {
 
     /**
-     * 过滤掉定时任务的 SQL
+          * 过滤掉定时任务的 SQL
      */
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
